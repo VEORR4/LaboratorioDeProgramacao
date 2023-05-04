d4b0a45433bfcb8d7f2707388a3f2942d86488c2
João Vitor Tranzil Amaral De Souza Galvão <128003170+VEORR4@users.noreply.github.com>
2023-05-04 19:20:50 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,14 @@
+
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+if numero1 > numero2:
+    numero1, numero2 = numero2, numero1
+
+count = 0
+for num in range(numero1, numero2+1):
+    if num % 2 == 0:
+        count += 1
+
+print(f"Entre {numero1} e {numero2}, há {count} números pares.")
+
